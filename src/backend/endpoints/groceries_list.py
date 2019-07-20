9105a6d3c1e0b5bffbd1c218e94a603cfc82c2b7
¨Vuk <GlisovicVuk@gmail.com>
2019-07-20 15:07:01 +0200

    Added put option

--- src/backend/endpoints/groceries_list.py
+++ src/backend/endpoints/groceries_list.py
@@ -13,16 +13,6 @@ ns = api.namespace('groceries', description='Groceries operations.')
 @ns.route('/')
 class GroceriesCollection(swagger.Resource):
 
-    # @ns.expect(GroceriesModel, validate=True)  # for formatting input
-    # @ns.marshal_with(GroceriesModel)  # for formatting output
-    # def post(self):
-    #     """Add products to the groceries list.
-    #     """
-    #     data = request.json
-    #     product = data[NAME]
-    #     info = data[INFO]
-    #     return app.grocerieslist.add(product, **info), 200
-
     @ns.marshal_with(GroceriesModel)
     def get(self):
         """Returns groceries list.
@@ -33,16 +23,7 @@ class GroceriesCollection(swagger.Resource):
 @ns.route('/<string:product>')
 class GroceriesItem(swagger.Resource):
 
-    # @ns.expect(groceries_arguments)  # for formatting input
-    # @ns.marshal_with(GroceriesModel)  # for formatting output
-    # def post(self, product):
-    #     """Add products to the groceries list. Can also add to existing products.
-    #     """
-    #     args = groceries_arguments.parse_args(request)
-    #     quantity = args.get('quantity')
-    #     return app.grocerieslist.add(product, quantity), 200
-
-    @ns.expect(GroceriesInfoModel, validate=True)  # for formatting input
+    @ns.expect(GroceriesInfoModel)  # for formatting input
     @ns.marshal_with(GroceriesModel)  # for formatting output
     def post(self, product):
         """Add products to the groceries list.
@@ -50,15 +31,22 @@ class GroceriesItem(swagger.Resource):
         info = request.json
         return app.grocerieslist.add(product, **info), 200
 
+    @ns.expect(GroceriesInfoModel)
+    @ns.marshal_with(GroceriesModel)
+    def put(self, product):
+        """Update product info on the groceries list.
+        """
+        info = request.json
+        return app.grocerieslist.update(product, info), 200
+
     @ns.marshal_with(GroceriesModel)
     def get(self, product):
         """Get one product.
         """
         return app.grocerieslist.get_product(product)
 
-    @ns.expect(GroceriesModel, validate=True)
     @ns.marshal_with(GroceriesModel)
     def delete(self, product):
         """Deletes a groceries from list.
         """
-        return app.grocerieslist.remove(**api.payload)
+        return app.grocerieslist.remove(product)
--- src/shopping/groceries.py
+++ src/shopping/groceries.py
@@ -13,28 +13,22 @@ class GroceriesList(object):
             self.add('peanut butter', 5)
 
     def add(self, name, quantity, **kwargs):
-        if any([dct.get(name) for dct in self.groceries]):
-            raise BadRequest("Product '{}' already exists.".format(name))
+        self.check_product_exists(name, should_exist=False)
         logging.info("Adding %s times '%s' to the groceries list.", quantity, name)
         new_item = {NAME: name, INFO: {QUANTITY: quantity}}
         self.groceries.append(new_item)
         return new_item
 
-    def remove(self, name, quantity):
-        logging.info("Removing %s times '%s' from the groceries list.", quantity, name)
-        if not self.groceries.get(name):
-            raise BadRequest("Product '{}' is not in the shopping list.".format(name))
-        product = self.groceries[name]
-        resulting_quantity = product[QUANTITY] - quantity
-        if resulting_quantity < 0:
-            raise BadRequest("Cannot remove this quantity; removing this amount results in a negative amount of '{}' on the list.".format(
-                name
-            ))
-        elif resulting_quantity == 0:
-            self.groceries.pop(name)
-        else:
-            self.groceries[name][QUANTITY] -= quantity
-        return {NAME: name, QUANTITY: resulting_quantity}
+    def update(self, name, info):
+        product_dict = self.check_product_exists(name, should_exist=True)
+        # changes original object by reference
+        product_dict[INFO] = info
+        return product_dict
+
+    def remove(self, name):
+        products_dict = self.check_product_exists(name, should_exist=True)
+        self.groceries.remove(products_dict)
+        return products_dict
 
     def get_list(self):
         logging.info("Listing all groceries.")
@@ -42,7 +36,17 @@ class GroceriesList(object):
 
     def get_product(self, product):
         logging.info("Getting product '{}'.".format(product))
-        product_list = [dct for dct in self.groceries if dct[NAME] == product]
-        if not any(product_list):
-            raise NotFound("No such product on shopping list: '{}'.".format(product))
-        return product_list[0]
+        product_dict = self.check_product_exists(product, should_exist=True)
+        return product_dict
+
+    def check_product_exists(self, name, should_exist=True):
+        product_list = [dct for dct in self.groceries if dct[NAME] == name]
+        if should_exist:
+            if not any(product_list):
+                raise NotFound("No such product on shopping list: '{}'.".format(name))
+            if len(product_list) > 1:
+                logging.warning("More than one product with name '%s' found", name)
+            return product_list[0]
+        else:
+            if any(product_list):
+                raise BadRequest("Product '{}' already exists.".format(name))

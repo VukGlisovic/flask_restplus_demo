256555c23bbcdcba48541b736d0f5d8e02423903
¨Vuk <GlisovicVuk@gmail.com>
2019-07-21 19:59:01 +0200

    cleaned up some code

--- src/app.py
+++ src/app.py
@@ -3,12 +3,9 @@ from flask import Flask, Blueprint
 from src.backend.restplus import api
 from src.shopping.groceries import GroceriesList
 from src import settings
-from src.backend.endpoints.groceries_list import ns as ns_groceries_list
+from src.backend.endpoints.namespace_groceries import ns as ns_groceries_list
+from src.constants import URL_PREFIX
 
-# from rest_api_demo import settings
-# from rest_api_demo.api.blog.endpoints.posts import ns as blog_posts_namespace
-# from rest_api_demo.api.blog.endpoints.categories import ns as blog_categories_namespace
-# from rest_api_demo.api.restplus import api
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -26,30 +23,20 @@ def configure_app(flask_app):
     flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
     flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
 
-    # flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
-    # flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
-
 
 def initialize_app(flask_app):
+    logging.info("Configuring app...")
     configure_app(flask_app)
-
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
+    logging.info("Setting up API...")
+    blueprint = Blueprint('api', __name__, url_prefix=URL_PREFIX)
     api.init_app(blueprint)
-    # api.add_namespace(blog_posts_namespace)
-    # api.add_namespace(blog_categories_namespace)
     api.add_namespace(ns_groceries_list)
     flask_app.register_blueprint(blueprint)
 
-    # db.init_app(flask_app)
-
 
 configure_app(app)
-logging.info("Initializing app...")
-blueprint = Blueprint('api', __name__, url_prefix='/api')
-api.init_app(blueprint)
-api.add_namespace(ns_groceries_list)
-app.register_blueprint(blueprint)
-logging.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
+initialize_app(app)
+logging.info('>>>>> Starting development server at http://{}{}/ <<<<<'.format(app.config['SERVER_NAME'], URL_PREFIX))
 
 
 if __name__ == "__main__":

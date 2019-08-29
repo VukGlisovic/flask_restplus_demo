db801468cb8bc868299248b8ba3209c13a7c1001
¨Vuk <GlisovicVuk@gmail.com>
2019-08-29 21:49:30 +0200

    Added two fields to groceries info model

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,8 +1,20 @@
 from src.backend.restplus import api
 import flask_restplus as swagger
+import datetime as dt
 
 
-groceries_info_fields = {'quantity': swagger.fields.Integer(required=True, min=1, description='How many of this product should be bought.')}
+groceries_info_fields = {'quantity': swagger.fields.Integer(required=True,
+                                                            min=1,
+                                                            description='How many of this product should be bought.',
+                                                            example=1),
+                         'price': swagger.fields.Float(required=True,
+                                                       min=0.,
+                                                       description='Price of single unit of the product.',
+                                                       example=1.5),
+                         'timestamp': swagger.fields.DateTime(required=False,
+                                                              default=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                                                              description='Time of buying.',
+                                                              example=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))}
 GroceriesInfoModel = api.model('GroceriesInfo', groceries_info_fields)
 
 groceries_fields = {'name': swagger.fields.String(required=True, description='The product name (the unique identifier).'),

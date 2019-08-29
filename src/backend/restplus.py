5b01b28e366f2c4005b852b679be877e9558e9ef
¨Vuk <GlisovicVuk@gmail.com>
2019-08-29 22:03:24 +0200

    validating datetime inputs

--- src/backend/restplus.py
+++ src/backend/restplus.py
@@ -1,11 +1,13 @@
 import logging
 from flask_restplus import Api
 from src import settings
+from jsonschema import FormatChecker
 
 
 api = Api(version='1.0',
           title='Groceries API',
-          description='For creating a shopping list for the groceries.')
+          description='For creating a shopping list for the groceries.',
+          format_checker=FormatChecker(formats=['date-time']))  # add the jsonschema formatcheckers here
 
 
 @api.errorhandler

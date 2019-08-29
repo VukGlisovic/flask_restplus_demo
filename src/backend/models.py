bacd2112010a5b147c6281799bbc27c7372293ac
¨Vuk <GlisovicVuk@gmail.com>
2019-08-29 21:51:10 +0200

    refactored to use of constants in field names

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,22 +1,23 @@
 from src.backend.restplus import api
+from src.constants import *
 import flask_restplus as swagger
 import datetime as dt
 
 
-groceries_info_fields = {'quantity': swagger.fields.Integer(required=True,
-                                                            min=1,
-                                                            description='How many of this product should be bought.',
-                                                            example=1),
-                         'price': swagger.fields.Float(required=True,
-                                                       min=0.,
-                                                       description='Price of single unit of the product.',
-                                                       example=1.5),
-                         'timestamp': swagger.fields.DateTime(required=False,
-                                                              default=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-                                                              description='Time of buying.',
-                                                              example=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))}
+groceries_info_fields = {QUANTITY: swagger.fields.Integer(required=True,
+                                                          min=1,
+                                                          description='How many of this product should be bought.',
+                                                          example=1),
+                         PRICE: swagger.fields.Float(required=True,
+                                                     min=0.,
+                                                     description='Price of single unit of the product.',
+                                                     example=1.5),
+                         TIMESTAMP: swagger.fields.DateTime(required=False,
+                                                            default=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+                                                            description='Time of buying.',
+                                                            example=dt.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))}
 GroceriesInfoModel = api.model('GroceriesInfo', groceries_info_fields)
 
-groceries_fields = {'name': swagger.fields.String(required=True, description='The product name (the unique identifier).'),
-                    'info': swagger.fields.Nested(GroceriesInfoModel)}
+groceries_fields = {NAME: swagger.fields.String(required=True, description='The product name (the unique identifier).'),
+                    INFO: swagger.fields.Nested(GroceriesInfoModel)}
 GroceriesModel = api.model('Groceries', groceries_fields)
